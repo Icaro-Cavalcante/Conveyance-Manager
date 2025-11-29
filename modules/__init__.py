e479dbacba38b4ec484c0bfb7381731228f9fe4c
Icaro-Cavalcante <icarodev06@gmail.com>
2025-11-29 12:37:43 -0300

    __init__
    > Uso do __init__ para transformar o diretório em um pacote

---
+++ modules/__init__.py
@@ -0,0 +1,8 @@
+from .abastecimentos import Abastecimento
+from .alocacao import Alocacao
+from .manutencoes import Manutencoes
+from .motoristas import *
+from .relatorios import Relatorio
+from .veiculos import *
+
+__all__ = ["Abastecimento", "Alocacao", "Manutencoes", "Cadastro_veiculos", "Veiculos", "Motoristas", "Cadstro_Motoristas", "Relatorio"]
