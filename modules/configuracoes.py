4e19fbf54f5a75b6ac471971c6dc0d5a702fec48
Icaro-Cavalcante <icarodev06@gmail.com>
2025-12-16 10:48:57 -0300

    Configurações organizadas
    - `Módulo de configurações organizadas em métodos bem divididos`
    - `Resolução de problemas em algumas funções`
    - `Polimento do código que envolve as configurações`

--- modules/configuracoes.py
+++ modules/configuracoes.py
@@ -2,116 +2,128 @@ import json
 caminho_json = r"config\settings.json"
 
 class Configuracao:
-    def mudar_configuracao(atributo):
-        if atributo == 1:
-            preco = float(input("Qual o novo preço da gasolina? "))
-            with open(caminho_json, "r") as f:
-                dados = json.load(f)
-                dados["configs"]["abastecimentos"]["valor_gasolina"] = preco
+    def configurar_gasolina():
+        preco = float(input("\nQual o novo preço da gasolina? "))
+        with open(caminho_json, "r") as f:
+            dados = json.load(f)
+            dados["configs"]["abastecimentos"]["valor_gasolina"] = preco
 
-            with open(caminho_json, "w") as f:
-                json.dump(dados, f, indent=4)
+        with open(caminho_json, "w") as f:
+            json.dump(dados, f, indent=4)
+        print("Preço alterado.\n")
 
-        elif atributo == 2:
-            escolha = str(input("1 - Peso por tipo de veículo\n2 - Custo por tipo de manutenção\n"))
-            if escolha == 1:
-                veiculo = str(input("Diga o tipo de veículo: "))
-                veiculo = veiculo.lower()
-                if veiculo == "carro" or veiculo == "moto" or veiculo == "caminhao":
-                    peso = int(input(f"Qual o novo peso do {veiculo}? "))
-                    with open(caminho_json, "r") as f:
-                        dados = json.load(f)
-                    dados["configs"]["manutencoes"]["peso"][veiculo] = peso
-                    with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
-                        print("Peso, alterado.")
-                else:
-                    print("Tipo de veículo inválido.")
-            elif escolha == 2:
-                print("1 - Preventiva\n2 - Corretiva")
-                tipo = int(input("Diga o tipo de manutenção: "))
-                if tipo == 1:
-                    preco = float(input("Qual o valor? "))
-                    with open(caminho_json, "w") as f:
-                        dados = json.load(f)
-                    dados["configs"]["manutencoes"]["custo"]["preventiva"] = preco
-                    with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
-                elif tipo == 2:
-                    preco = float(input("Qual o valor? "))
-                    with open(caminho_json, "w") as f:
-                        dados = json.load(f)
-                    dados["configs"]["manutencoes"]["custo"]["corretiva"] = preco
-                    with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
-                else:
-                    print("Escolha inválida.")
-
-        elif atributo == 3:
-            print("1 - Moto\n2 - Carro\n3 - Caminhão")
-            escolha = int(input("Qual a sua escolha? "))
-            if escolha == 1:
-                cnh = str(input("Qual a nova categoria de CNH da moto? "))
-                cnh = cnh.upper()
+    def configurar_manutencao():
+        print("\n1 - Peso por tipo de veículo\n2 - Custo por tipo de manutenção\n")
+        escolha = int(input("Escolha o que deseja configurar: "))
+        if escolha == 1:
+            veiculo = str(input("\nDiga o tipo de veículo: "))
+            veiculo = veiculo.lower()
+            if veiculo == "carro" or veiculo == "moto" or veiculo == "caminhao":
+                peso = int(input(f"Qual o novo peso do {veiculo}? "))
                 with open(caminho_json, "r") as f:
                     dados = json.load(f)
-                dados["configs"]["compatibilidade"]["moto"] = cnh
+                dados["configs"]["manutencoes"]["peso"][veiculo] = peso
                 with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
-            elif escolha == 2:
-                cnh = str(input("Qual a nova categoria de CNH do carro? "))
-                cnh = cnh.upper()
-                with open(caminho_json, "r") as f:
-                    dados = json.load(f)
-                dados["configs"]["compatibilidade"]["carro"] = cnh
-                with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
-            elif escolha == 3:
-                cnh = str(input("Qual a nova categoria de CNH do caminhão? "))
-                cnh = cnh.upper()
-                with open(caminho_json, "r") as f:
-                    dados = json.load(f)
-                dados["configs"]["compatibilidade"]["caminhao"] = cnh
-                with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
+                    json.dump(dados, f, indent=4)
+                    print("Peso, alterado.\n")
             else:
-                print("Escolha inválida.")
+                print("Tipo de veículo inválido.\n")
+        elif escolha == 2:
+            print("\n1 - Preventiva\n2 - Corretiva")
+            tipo = int(input("Diga o tipo de manutenção: "))
+            if tipo == 1:
+                preco = float(input("Qual o valor? "))
+                with open(caminho_json, "r") as f:
+                    dados = json.load(f)
+                dados["configs"]["manutencoes"]["custo"]["preventiva"] = preco
+                with open(caminho_json, "w") as f:
+                    json.dump(dados, f, indent=4)
+                print("Preço alterado.\n")
+            elif tipo == 2:
+                preco = float(input("Qual o valor? "))
+                with open(caminho_json, "r") as f:
+                    dados = json.load(f)
+                dados["configs"]["manutencoes"]["custo"]["corretiva"] = preco
+                with open(caminho_json, "w") as f:
+                    json.dump(dados, f, indent=4)
+                print("Preço alterado.\n")
+            else:
+                print("Escolha inválida.\n")
 
-        elif atributo == 4:
-            limite = float(input("Digite o novo limite de quilometragem para manuteção: "))
+    def configurar_compatibilidade():
+        print("\n1 - Moto\n2 - Carro\n3 - Caminhão")
+        escolha = int(input("Qual a sua escolha? "))
+        if escolha == 1:
+            cnh = str(input("Qual a nova categoria de CNH da moto? "))
+            cnh = cnh.upper()
             with open(caminho_json, "r") as f:
                 dados = json.load(f)
-            dados["configs"]["quilometragem"]["limite"] = limite
+            dados["configs"]["compatibilidade"]["moto"] = cnh
             with open(caminho_json, "w") as f:
                     json.dump(dados, f, indent=4)
+            print("Compatibilidade alterada.\n")
+        elif escolha == 2:
+            cnh = str(input("Qual a nova categoria de CNH do carro? "))
+            cnh = cnh.upper()
+            with open(caminho_json, "r") as f:
+                dados = json.load(f)
+            dados["configs"]["compatibilidade"]["carro"] = cnh
+            with open(caminho_json, "w") as f:
+                    json.dump(dados, f, indent=4)
+            print("Compatibilidade alterada.\n")
+        elif escolha == 3:
+            cnh = str(input("Qual a nova categoria de CNH do caminhão? "))
+            cnh = cnh.upper()
+            with open(caminho_json, "r") as f:
+                dados = json.load(f)
+            dados["configs"]["compatibilidade"]["caminhao"] = cnh
+            with open(caminho_json, "w") as f:
+                    json.dump(dados, f, indent=4)
+            print("Compatibilidade alterada.\n")
+        else:
+            print("Escolha inválida.\n")
 
-        elif atributo == 5:
-            print("1 - Carro\n2 - Moto\n3 - Caminhão")
-            escolha = int(input("Qual sua escolha?"))
-            if escolha == 1:
-                minimo = float(input("Qual o consumo minimo? "))
-                maximo = float(input("Qual o consumo maximo? "))
-                with open(caminho_json, "r") as f:
-                    dados = json.load(f)
-                dados["configs"]["abastecimentos"]["consumo_medio"]["carro"]["minimo"] = minimo
-                dados["configs"]["abastecimentos"]["consumo_medio"]["carro"]["maximo"] = maximo
-                with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
-            if escolha == 2:
-                minimo = float(input("Qual o consumo minimo? "))
-                maximo = float(input("Qual o consumo maximo? "))
-                with open(caminho_json, "r") as f:
-                    dados = json.load(f)
-                dados["configs"]["abastecimentos"]["consumo_medio"]["moto"]["minimo"] = minimo
-                dados["configs"]["abastecimentos"]["consumo_medio"]["moto"]["maximo"] = maximo
-                with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
-            if escolha == 3:
-                minimo = float(input("Qual o consumo minimo? "))
-                maximo = float(input("Qual o consumo maximo? "))
-                with open(caminho_json, "r") as f:
-                    dados = json.load(f)
-                dados["configs"]["abastecimentos"]["consumo_medio"]["caminhao"]["minimo"] = minimo
-                dados["configs"]["abastecimentos"]["consumo_medio"]["caminhao"]["maximo"] = maximo
-                with open(caminho_json, "w") as f:
-                        json.dump(dados, f, indent=4)
+    def configurar_limite():
+        limite = float(input("\nDigite o novo limite de quilometragem para manuteção: "))
+        with open(caminho_json, "r") as f:
+            dados = json.load(f)
+        dados["configs"]["quilometragem"]["limite"] = limite
+        with open(caminho_json, "w") as f:
+                json.dump(dados, f, indent=4)
+        print("Limite alterado.\n")
+
+    def configurar_consumo_padrao():
+        print("\n1 - Carro\n2 - Moto\n3 - Caminhão")
+        escolha = int(input("Qual sua escolha?"))
+        if escolha == 1:
+            minimo = float(input("Qual o consumo minimo? "))
+            maximo = float(input("Qual o consumo maximo? "))
+            with open(caminho_json, "r") as f:
+                dados = json.load(f)
+            dados["configs"]["abastecimentos"]["consumo_medio"]["carro"]["minimo"] = minimo
+            dados["configs"]["abastecimentos"]["consumo_medio"]["carro"]["maximo"] = maximo
+            with open(caminho_json, "w") as f:
+                    json.dump(dados, f, indent=4)
+            print("Consumo médio alterado.")
+        if escolha == 2:
+            minimo = float(input("Qual o consumo minimo? "))
+            maximo = float(input("Qual o consumo maximo? "))
+            with open(caminho_json, "r") as f:
+                dados = json.load(f)
+            dados["configs"]["abastecimentos"]["consumo_medio"]["moto"]["minimo"] = minimo
+            dados["configs"]["abastecimentos"]["consumo_medio"]["moto"]["maximo"] = maximo
+            with open(caminho_json, "w") as f:
+                    json.dump(dados, f, indent=4)
+            print("Consumo médio alterado.")
+        if escolha == 3:
+            minimo = float(input("Qual o consumo minimo? "))
+            maximo = float(input("Qual o consumo maximo? "))
+            with open(caminho_json, "r") as f:
+                dados = json.load(f)
+            dados["configs"]["abastecimentos"]["consumo_medio"]["caminhao"]["minimo"] = minimo
+            dados["configs"]["abastecimentos"]["consumo_medio"]["caminhao"]["maximo"] = maximo
+            with open(caminho_json, "w") as f:
+                    json.dump(dados, f, indent=4)
+            print("Consumo médio alterado.")
+        else:
+             print("Escolha inválida.\n")
